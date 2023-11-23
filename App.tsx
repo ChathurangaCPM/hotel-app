b85bda8423bdfe51733f124d7d804a95bf9f9647
cpmchathuranga <chathurangacpm@gmail.com>
2023-11-23 19:15:37 +0530

    add new UI framework

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-// import MainNavigator from './navigation/main-navigator';
+import {NativeBaseProvider} from 'native-base';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import MainNavigationContainer from './src/navigation/navigation-container';
 
 const App = () => {
   return (
     <GestureHandlerRootView style={{flex: 1}}>
-      <MainNavigationContainer />
+      <NativeBaseProvider>
+        <MainNavigationContainer />
+      </NativeBaseProvider>
     </GestureHandlerRootView>
   );
 };
--- src/screens/auth/register.js
+++ src/screens/auth/register.js
@@ -1,6 +1,60 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {
+  Box,
+  Center,
+  Heading,
+  VStack,
+  FormControl,
+  Button,
+  Input,
+  KeyboardAvoidingView,
+} from 'native-base';
+import {Platform} from 'react-native';
 
 export default function Register() {
-  return <Text>Register</Text>;
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+      <Center w="100%">
+        <Box safeArea p="2" w="90%" py="8">
+          <Heading
+            size="lg"
+            color="coolGray.800"
+            _dark={{
+              color: 'warmGray.50',
+            }}
+            fontWeight="semibold">
+            Welcome
+          </Heading>
+          <Heading
+            mt="1"
+            color="coolGray.600"
+            _dark={{
+              color: 'warmGray.200',
+            }}
+            fontWeight="medium"
+            size="xs">
+            Sign up to continue!
+          </Heading>
+          <VStack space={3} mt="5">
+            <FormControl>
+              <FormControl.Label>Email</FormControl.Label>
+              <Input height={10} keyboardType="email-address" fontSize={16}/>
+            </FormControl>
+            <FormControl>
+              <FormControl.Label>Password</FormControl.Label>
+              <Input height={10} type="password" fontSize={16} />
+            </FormControl>
+            <FormControl>
+              <FormControl.Label>Confirm Password</FormControl.Label>
+              <Input height={10} type="password" fontSize={16} />
+            </FormControl>
+            <Button mt="2" colorScheme="indigo">
+              Sign up
+            </Button>
+          </VStack>
+        </Box>
+      </Center>
+    </KeyboardAvoidingView>
+  );
 }
