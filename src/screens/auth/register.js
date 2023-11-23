7f95d8cb10fcf21ff596f3c309c195ddbad27f8f
cpmchathuranga <chathurangacpm@gmail.com>
2023-11-23 19:49:22 +0530

    add admin registation page

--- src/screens/auth/register.js
+++ src/screens/auth/register.js
@@ -7,54 +7,94 @@ import {
   FormControl,
   Button,
   Input,
-  KeyboardAvoidingView,
+  HStack,
+  Flex,
+  Text,
 } from 'native-base';
-import {Platform} from 'react-native';
+import {
+  Dimensions,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  SafeAreaView,
+} from 'react-native';
+
+const {width} = Dimensions.get('screen');
 
 export default function Register() {
   return (
-    <KeyboardAvoidingView
-      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
-      <Center w="100%">
-        <Box safeArea p="2" w="90%" py="8">
-          <Heading
-            size="lg"
-            color="coolGray.800"
-            _dark={{
-              color: 'warmGray.50',
-            }}
-            fontWeight="semibold">
-            Welcome
-          </Heading>
-          <Heading
-            mt="1"
-            color="coolGray.600"
-            _dark={{
-              color: 'warmGray.200',
-            }}
-            fontWeight="medium"
-            size="xs">
-            Sign up to continue!
-          </Heading>
-          <VStack space={3} mt="5">
-            <FormControl>
-              <FormControl.Label>Email</FormControl.Label>
-              <Input height={10} keyboardType="email-address" fontSize={16}/>
-            </FormControl>
-            <FormControl>
-              <FormControl.Label>Password</FormControl.Label>
-              <Input height={10} type="password" fontSize={16} />
-            </FormControl>
-            <FormControl>
-              <FormControl.Label>Confirm Password</FormControl.Label>
-              <Input height={10} type="password" fontSize={16} />
-            </FormControl>
-            <Button mt="2" colorScheme="indigo">
-              Sign up
-            </Button>
-          </VStack>
-        </Box>
-      </Center>
-    </KeyboardAvoidingView>
+    <SafeAreaView style={{flex: 1}}>
+      <KeyboardAvoidingView
+        style={{flex: 1}}
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+        <ScrollView>
+          <Center w="100%">
+            <Box safeArea p="2" w="90%" py="8">
+              <Heading
+                size="lg"
+                color="coolGray.800"
+                _dark={{
+                  color: 'warmGray.50',
+                }}
+                fontWeight="semibold">
+                Welcome
+              </Heading>
+              <Heading
+                mt="1"
+                color="coolGray.600"
+                _dark={{
+                  color: 'warmGray.200',
+                }}
+                fontWeight="medium"
+                size="xs">
+                Create an Admin Account
+              </Heading>
+
+              <Text fontSize="xs" color="muted.600" mt={4}>
+                The admin account can access all hotel information and can
+                update all data related to hotel.
+              </Text>
+
+              <VStack space={3} mt="5">
+                <HStack space={3}>
+                  <FormControl flex={1}>
+                    <FormControl.Label>First name</FormControl.Label>
+                    <Input height={10} fontSize={16} size="lg" />
+                  </FormControl>
+                  <FormControl flex={1}>
+                    <FormControl.Label>Last name</FormControl.Label>
+                    <Input height={10} fontSize={16} />
+                  </FormControl>
+                </HStack>
+
+                <FormControl w="100%">
+                  <FormControl.Label>Email</FormControl.Label>
+                  <Input
+                    height={10}
+                    keyboardType="email-address"
+                    fontSize={16}
+                  />
+                </FormControl>
+                <FormControl>
+                  <FormControl.Label>Password</FormControl.Label>
+                  <Input height={10} type="password" fontSize={16} />
+                </FormControl>
+                <FormControl>
+                  <FormControl.Label>Confirm Password</FormControl.Label>
+                  <Input height={10} type="password" fontSize={16} />
+                </FormControl>
+                <Button
+                  mt="2"
+                  colorScheme="indigo"
+                  isLoading
+                  isLoadingText="Creating...">
+                  Create a Primary Account
+                </Button>
+              </VStack>
+            </Box>
+          </Center>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   );
 }
